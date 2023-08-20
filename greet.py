03112cd8735f34bc5237825fa9d7077a1260780a
Parallax <141752026+RudraCoders@users.noreply.github.com>
2023-08-20 22:35:28 +0530

    Add files via upload

---
+++ greet.py
@@ -0,0 +1,11 @@
+import time 
+t = int(time.strftime('%H'))
+if 0<t<=12:
+  print ("hlo sir\n Good morning")
+elif 12<t<=16:
+  print ("good afternoon sir")
+else:
+  print ("good evening sir")
+
+  
+ 
